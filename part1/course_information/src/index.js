680ecfe00a2a1614dce60319751257b6b2699dbc
villetuh <keymic@gmail.com>
2020-04-15 18:32:40 +0300

    Change course data to be contained in an object
    All information related to single course is now contained in course
    object.
    
    Exercises: 1.3, 1.4, 1.5

--- part1/course_information/src/index.js
+++ part1/course_information/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const Header = (props) => {
   return (
-  <h1>{props.course}</h1>
+  <h1>{props.courseName}</h1>
   )
 };
 
@@ -26,26 +26,27 @@ const Part = (props) => {
 };
 
 const Total = (props) => {
+  let numberOfExercises = props.parts.map(part => part.exercises).reduce((a, b) => a + b, 0);
   return (
-  <p>Number of exercises {props.numberOfExercises}</p>
+  <p>Number of exercises {numberOfExercises}</p>
   )
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+  };
   
-  let numberOfExercises = parts.map(part => part.exercises).reduce((a, b) => a + b, 0);
-
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total numberOfExercises={numberOfExercises} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
