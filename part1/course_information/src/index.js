a6b7f8e8df31e20e0a8b253215fddf87f5510601
villetuh <keymic@gmail.com>
2020-04-15 18:01:36 +0300

    Refactor to use separate components
    Split App-component to three smaller components which handle
    constructing their specific part of the page.
    
    Exercises: 1.1, 1.2

--- part1/course_information/src/index.js
+++ part1/course_information/src/index.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (props) => {
+  return (
+  <h1>{props.course}</h1>
+  )
+};
+
+const Content = (props) => {
+  return (
+    <>
+    {props.parts.map(part => (
+      <Part part={part} key={part.name} />
+    ))}
+    </>
+  )
+};
+
+const Part = (props) => {
+  return (
+    <p>
+        {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return (
+  <p>Number of exercises {props.numberOfExercises}</p>
+  )
+};
+
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ];
+  
+  let numberOfExercises = parts.map(part => part.exercises).reduce((a, b) => a + b, 0);
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total numberOfExercises={numberOfExercises} />
     </div>
   );
 };
