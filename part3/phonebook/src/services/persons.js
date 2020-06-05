c57130867415ce299755f9f98872e60583b13219
villetuh <keymic@gmail.com>
2020-06-05 15:12:16 +0300

    Change API address to point to deployed back end
    API address was set to full address of the back end deployment. It could
    have also been investigated if it would've been possible to use still
    localhost address but getting the port through PORT environmental
    variable.
    
    Exercise: 3.11

--- part3/phonebook/src/services/persons.js
+++ part3/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = 'https://phonebook-vvt.herokuapp.com/api/persons';
 
 const getAll = () => {
   const response = axios.get(baseUrl);
