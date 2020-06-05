19ef25359b2e36bcaba671fab5edabb784a9753c
villetuh <keymic@gmail.com>
2020-06-05 15:06:27 +0300

    Add support for serving static pages
    Back end will now try to serve static content from build folder if no
    matching route can be found. This is done so that back end and React app
    can be installed easily to same server.
    
    Exercise: 3.11

--- part3/phonebook_backend/src/index.js
+++ part3/phonebook_backend/src/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('post-data', request => {
   if (request.method !== 'POST') {
