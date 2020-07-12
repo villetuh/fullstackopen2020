e1615af7c576fed71d1a704c99a135e76f9378be
villetuh <keymic@gmail.com>
2020-07-12 14:19:58 +0300

    Add option to create new users
    Created new Users controller which allows creation of new users using
    HTTP POST.
    
    Exercise: 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware');
 
@@ -17,6 +18,7 @@ app.use(express.static('build'));
 app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.errorHandler);
 
---
+++ part4/bloglist/tests/users_api.test.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+
+const app = require('../app');
+const User = require('../models/user');
+const helper = require('./test_helper');
+
+const api = supertest(app);
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({
+      username: 'testuser',
+      name: 'Teemu Testaaja',
+      passwordHash
+    });
+
+    await user.save();
+  });
+
+  test('adding a new user with unique username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'testuser2',
+      name: 'Tiina Testaaja',
+      password: 'abbaabc'
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map(u => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  afterAll(async () => {
+    mongoose.connection.close();
+  });
+});
