42d2e5371ecd1461b450af00ee2f8fdf36294ad9
villetuh <keymic@gmail.com>
2020-09-10 19:13:34 +0300

    Add login form
    User needs now log in before list of blogs is displayed. Currently login
    doesn't persist.
    
    Exercise: 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
+import Login from './components/Login';
+
 import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +17,41 @@ const App = () => {
     )  
   }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  );
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log('Wrong credentials');
+    }
+  }
+
+  if (user === null) {
+    return(
+      <Login
+        username={username}
+        password={password}
+        handleLogin={handleLogin}
+        setUsername={setUsername}
+        setPassword={setPassword}
+      />
+    );
+  }
+  else {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <div>{user.name} logged in</div>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    );
+  }
 };
 
 export default App;
---
+++ part5/bloglist-frontend/src/components/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Login = ({username, password, handleLogin, setUsername, setPassword}) => (
+  <div>
+    <h2>Log in to the application</h2>
+    <form onSubmit={handleLogin}>
+      <div>
+        username:
+        <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+      </div>
+      <div>
+        password:
+        <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+);
+
+export default Login;
