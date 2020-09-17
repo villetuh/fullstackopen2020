ce6ba3a6227efb09d401e307d6b2beba50a6e83c
villetuh <keymic@gmail.com>
2020-09-17 16:12:18 +0300

    Update Blog component to show more information
    Blog component displays additional information about blog. Like button
    was added which will allow rating the blogs but it doesn't currently
    have any functionality.
    
    Exercise: 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  return (
+  <div style={blogStyle}>
+    {blog.title}<button onClick={toggleDetails}>{showDetails ? 'hide' : 'view'}</button>
+    { showDetails &&
+      <div>
+      url: {blog.url} <br />
+      likes: {blog.likes} <button>like</button> <br />
+      {blog.author}
+    </div>
+    }
   </div>
-);
+)};
 
 export default Blog;
