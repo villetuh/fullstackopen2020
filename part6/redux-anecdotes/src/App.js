45dfab4373bf9ec1c621fb0e07a87cd9b36ca425
villetuh <keymic@gmail.com>
2020-10-18 12:20:46 +0300

    Fetch initial anecdotes from back end
    Changed fetching of initial anecdotes to happen from back end instead of
    using hard coded values in anecdoteReducer.
    
    Exercise: 6.13

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Notification from './components/Notification';
 
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
+import anecdoteService from './services/anecdotes';
+
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)));
+  }, [dispatch]);
+
   return (
     <div>
       <Notification />
