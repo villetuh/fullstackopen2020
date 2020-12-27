db22c67f58f7647d4a18f37fc3ac0d966f2ba161
villetuh <keymic@gmail.com>
2020-12-27 14:37:19 +0200

    Allow querying books by author
    Modified query to retrieve all books to allow for optional author
    argument which can be used to retrieve only books by specific author.
    
    Exercise: 8.4

--- part8/graphql_tests/library-backend.js
+++ part8/graphql_tests/library-backend.js
@@ -97,7 +97,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!,
     authorCount: Int!,
-    allBooks: [Book!],
+    allBooks(author: String): [Book!],
     allAuthors: [Author!]
   }
 `;
@@ -106,7 +106,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => !args.author ? books : books.filter(book => book.author === args.author),
     allAuthors: () => authors
   },
   Author: {
