a5a85be5878a1b1bca5a89d9ca45ab5f9b5ef16a
villetuh <keymic@gmail.com>
2020-12-27 15:05:58 +0200

    Add mutation to add new book
    New book can be added using addBook mutation. If book is by author that
    isn't already found from data it will be added.
    
    Exercise: 8.6

--- part8/graphql_tests/library-backend.js
+++ part8/graphql_tests/library-backend.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { v1: uuid } = require('uuid');
 
 let authors = [
   {
@@ -100,6 +101,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!],
     allAuthors: [Author!]
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+  }
 `;
 
 const resolvers = {
@@ -116,6 +126,18 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter(book => book.author === root.name).length;
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+
+      if (authors.find(author => author.name === args.author) === undefined) {
+        const newAuthor = { name: args.author, id: uuid() };
+        authors = authors.concat(newAuthor);
+      }
+      return book;
     }
   }
 };
