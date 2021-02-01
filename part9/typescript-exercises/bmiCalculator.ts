1c6ada477e363b12f86828545687869dde5ab8f4
villetuh <keymic@gmail.com>
2021-02-01 16:42:19 +0200

    Add command line support
    BMI and exercise calculators now support passing required values as
    command line arguments.
    
    Exercise: 9.3

--- part9/typescript-exercises/bmiCalculator.ts
+++ part9/typescript-exercises/bmiCalculator.ts
@@ -1,27 +1,47 @@
+interface PersonDetails {
+  heightInCm: number,
+  weightInKg: number
+}
+
 const calculateBmi = (heightInCm: number, weightInKg: number): string => {
-    // BMI = weightInKg / heightInM^2
-    const bmiValue = weightInKg / Math.pow(heightInCm / 100, 2);
-    return translateBmiValueToCategory(bmiValue);
+  // BMI = weightInKg / heightInM^2
+  const bmiValue = weightInKg / Math.pow(heightInCm / 100, 2);
+  return translateBmiValueToCategory(bmiValue);
 };
 
 const translateBmiValueToCategory = (value: number): string => {
-    if (value < 15) {
-        return 'Very severly underweight';
-    } else if (value >= 15 && value < 16) {
-        return 'Severly underweight';
-    } else if (value >= 16 && value < 18.5) {
-        return 'Underweight';
-    } else if (value >= 18.5 && value < 25) {
-        return 'Normal (healthy weight)';
-    } else if (value >= 25 && value < 30) {
-        return 'Overweight';
-    } else if (value >= 30 && value < 35) {
-        return 'Obese Class I (Moderately obese)';
-    } else if (value >= 35 && value < 40) {
-        return 'Obese Class II (Moderately obese)';
-    } else {
-        return 'Obese Class III (Moderately obese)';
-    }
+  if (value < 15) {
+    return 'Very severly underweight';
+  } else if (value >= 15 && value < 16) {
+    return 'Severly underweight';
+  } else if (value >= 16 && value < 18.5) {
+    return 'Underweight';
+  } else if (value >= 18.5 && value < 25) {
+    return 'Normal (healthy weight)';
+  } else if (value >= 25 && value < 30) {
+    return 'Overweight';
+  } else if (value >= 30 && value < 35) {
+    return 'Obese Class I (Moderately obese)';
+  } else if (value >= 35 && value < 40) {
+    return 'Obese Class II (Moderately obese)';
+  } else {
+    return 'Obese Class III (Moderately obese)';
+  }
 };
 
-console.log(calculateBmi(180, 74));
+const parseBMIArguments = (args: Array<string>): PersonDetails => {
+  if (args.length < 4 || args.length > 4) {
+    throw new Error('Incorrect number of arguments provided.');
+  }
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      heightInCm: Number(args[2]),
+      weightInKg: Number(args[3])
+    }
+  } else {
+    throw new Error('Invalid arguments. Arguments must be numbers');
+  }
+}
+
+const personDetails = parseBMIArguments(process.argv);
+console.log(calculateBmi(personDetails.heightInCm, personDetails.weightInKg));
--- part9/typescript-exercises/exerciseCalculator.ts
+++ part9/typescript-exercises/exerciseCalculator.ts
@@ -1,37 +1,66 @@
 interface ExerciseInformation {
-    periodLength: number,
-    trainingDays: number,
-    target: number,
-    average: number,
-    rating: number,
-    ratingDescription: string,
-    success: boolean
+  periodLength: number,
+  trainingDays: number,
+  target: number,
+  average: number,
+  rating: number,
+  ratingDescription: string,
+  success: boolean
 }
 
-const calculateExercises = (dailyExerciseAmounts: Array<number>): ExerciseInformation => {
-    const averageDailyAmount = dailyExerciseAmounts.reduce((prev, current) => prev + current) / dailyExerciseAmounts.length;
-    const targetDailyAmount = 2.0;
+interface ExerciseArguments {
+  target: number,
+  dailyExerciseAmounts: Array<number>
+}
 
-    const averageToTargetRatio = averageDailyAmount / targetDailyAmount
-    let rating = 2;
-    let ratingDescription = 'Good job! Target met.';
-    if (averageToTargetRatio > 1.25) {
-        rating = 3;
-        ratingDescription = 'Excellent! Target exceeded.';
-    } else if (averageToTargetRatio < 0.90) {
-        rating = 1;
-        ratingDescription = 'You can do better! Meet the target next time.';
-    }
+const calculateExercises = (targetDailyAmount: number, dailyExerciseAmounts: Array<number>): ExerciseInformation => {
+  const averageDailyAmount = dailyExerciseAmounts.reduce((prev, current) => prev + current) / dailyExerciseAmounts.length;
 
-    return {
-        periodLength: dailyExerciseAmounts.length,
-        trainingDays: dailyExerciseAmounts.filter(x => x > 0).length,
-        target: targetDailyAmount,
-        average: averageDailyAmount,
-        rating: rating,
-        ratingDescription: ratingDescription,
-        success: targetDailyAmount >= averageDailyAmount
-    }
+  const averageToTargetRatio = averageDailyAmount / targetDailyAmount
+  let rating = 2;
+  let ratingDescription = 'Good job! Target met.';
+  if (averageToTargetRatio > 1.25) {
+    rating = 3;
+    ratingDescription = 'Excellent! Target exceeded.';
+  } else if (averageToTargetRatio < 0.90) {
+    rating = 1;
+    ratingDescription = 'You can do better! Meet the target next time.';
+  }
+
+  return {
+    periodLength: dailyExerciseAmounts.length,
+    trainingDays: dailyExerciseAmounts.filter(x => x > 0).length,
+    target: targetDailyAmount,
+    average: averageDailyAmount,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    success: targetDailyAmount >= averageDailyAmount
+  }
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1]));
+const parseExerciseArguments = (args: Array<string>): ExerciseArguments => {
+  if (args.length < 4 || args.length > 4) {
+    throw new Error('Incorrect number of arguments provided.');
+  }
+
+  const targetAmount = Number(args[2]);
+  if (isNaN(targetAmount)) {
+    throw new Error('Invalid arguments. Target argument must be a number');
+  } 
+
+  const exercises = args[3].replace('[', '').replace(']', '').replace(' ', '').split(',').map(x => Number(x));
+
+  for (let i = 0; i < exercises.length; i++) {
+    if (exercises[i] === NaN) {
+      throw new Error('Invalid arguments. Daily exercise argument must contain array of numbers');
+    }
+  }
+
+  return {
+    target: targetAmount,
+    dailyExerciseAmounts: exercises
+  }
+}
+
+const exerciseArguments = parseExerciseArguments(process.argv);
+console.log(calculateExercises(exerciseArguments.target, exerciseArguments.dailyExerciseAmounts));
