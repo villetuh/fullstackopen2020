6764d5f9b32a192bf38648b6d67a58e92e6cd6f3
villetuh <keymic@gmail.com>
2021-02-08 18:30:23 +0200

    Add route to retrieve all patients
    GET request to /api/patients/ returns all registered patients.
    
    Exercise: 9.11

---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,24 @@
+import patientData from '../../data/patients.json';
+
+import { NonSensitivePatientEntry, PatientEntry } from '../types';
+
+const patients: Array<PatientEntry> = patientData;
+
+const getEntries = (): Array<PatientEntry> => {
+  return patients;
+};
+
+const getNonSensitiveEntries = (): Array<NonSensitivePatientEntry> => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation
+  }));
+};
+
+export default {
+  getEntries,
+  getNonSensitiveEntries
+};
